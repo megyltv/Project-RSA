a0ad733c297146bc40c7f6eb5d7008ca7d7316bc
Megan Toinga <megan.toinga@concordia.ca>
2021-02-28 18:47:10 -0500

    MeganToinga | Generates N and Phi(N). Refactors prime code

---
+++ ProjectRSA/PrimeExtensions.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ProjectRSA
+{
+    /// <summary>
+    /// Determine if the number is prime using Miller-Rabin algorithm
+    /// </summary>
+    public static class PrimeExtensions
+    {
+        /// <summary>
+        ///  n = 2^s*d + 1
+        /// </summary>
+        public static bool IsPrime(int number)
+        {
+            if (number <= 1 || number % 2 == 0) return false;
+            if (number <= 3) return true;
+
+            int d = number - 1;
+            d /= 2;
+
+            var numberOfRounds = 5;
+
+            for (var i = 0; i < numberOfRounds; i++)
+            {
+                int modularExponentiation = GetModularExponentiation(number, d);
+                if (modularExponentiation == 1 || modularExponentiation == number - 1) return true;
+
+                while (d != number - 1)
+                {
+                    modularExponentiation = (int)Math.Pow(modularExponentiation, 2) % number;
+                    d *= 2;
+
+                    if (IsComposite(modularExponentiation)) return false;
+                    if (modularExponentiation == number - 1) return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        ///  Generate random in the range of [2, n-2]
+        /// </summary>
+        private static int GetModularExponentiation(int number, int d)
+        {
+            var random = new Random().Next(2, number - 2);
+            var modularExponentiation = 1;
+            while (d > 0)
+            {
+                if (d % 2 == 1)
+                    modularExponentiation = (modularExponentiation * random) % number;
+                d /= 2;
+                random = (int)Math.Pow(random, 2) % number;
+            }
+
+            return modularExponentiation;
+        }
+
+        private static bool IsComposite(int modularExponentiation)
+        {
+            return modularExponentiation == 1;
+        }
+    }
+}
