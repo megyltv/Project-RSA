2f8fc4ab14c46ea62bd899ff049083e8851c6b1a
Megan Toinga <megan.toinga@concordia.ca>
2021-03-17 20:11:46 -0500

    MeganToinga | Implements method to converts message to chunk of integers

---
+++ ProjectRSA.UnitTests/Extensions/MessageExtensionsTests.cs
@@ -0,0 +1,24 @@
+﻿using ProjectRSA.Extensions;
+using System.Collections.Generic;
+using Xunit;
+
+namespace ProjectRSA.UnitTests.Extensions
+{
+    public class MessageExtensionsTests
+    {
+        [Fact]
+        public void ConvertToInt_ShouldConvertStringToChunkOfIntegers()
+        {
+            var message = "Hello World";
+
+            var chunks = MessageExtensions.ConvertToInt(message);
+
+            var expectedChunks = new List<int> { 4744556, 7106336, 5730162, 27748 };
+            Assert.Equal(expectedChunks.Count, chunks.Count);
+            for(var i=0; i<expectedChunks.Count; i++)
+            {
+                Assert.Equal(expectedChunks[i], chunks[i]);
+            }
+        }
+    }
+}
---
+++ ProjectRSA/Extensions/MessageExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectRSA.Extensions
+{
+    public class MessageExtensions
+    {
+       private const int SizeChunks = 3;
+
+       public static List<int> ConvertToInt(string message)
+       {
+            var messageChunks = SeparateMessageChunks(message);
+
+            var result = new List<int>();
+            foreach (var chunk in messageChunks)
+            {
+                var hexadecimals = Encoding.Default.GetBytes(chunk);
+                var hexadecimalString = BitConverter.ToString(hexadecimals);
+                hexadecimalString = hexadecimalString.Replace("-", "");
+                result.Add(int.Parse(hexadecimalString, System.Globalization.NumberStyles.HexNumber));
+            }
+
+            return result;
+        }
+
+        private static List<string> SeparateMessageChunks(string message)
+        {
+            var messageChunks = new List<string>();
+            var index = 0;
+            while (index + SizeChunks < message.Length)
+            {
+                messageChunks.Add(message.Substring(index, SizeChunks));
+                index += SizeChunks;
+            }
+            messageChunks.Add(message[index..]);
+
+            return messageChunks;
+        }
+    }
+}
